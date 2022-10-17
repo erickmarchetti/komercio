8375d0421cc583bdd6ae3dd8148fb01ed414a0bb
ErickMarchetti <erickgmarchetti@gmail.com>
2022-10-17 15:47:33 -0300

    feat: model user

--- users/models.py
+++ users/models.py
@@ -1,3 +1,14 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class User(AbstractUser):
+    username = models.TextField(unique=True)
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    is_seller = models.BooleanField(default=False)
+
+    REQUIRED_FIELDS: list[str] = [
+        "first_name",
+        "last_name",
+    ]
