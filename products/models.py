c356c50cb95f379b2c99743a68ec34ae8eb43f09
ErickMarchetti <erickgmarchetti@gmail.com>
2022-10-18 11:04:55 -0300

    feat: relacionamento 1:n com user

--- products/models.py
+++ products/models.py
@@ -6,3 +6,7 @@ class Product(models.Model):
     price = models.DecimalField(max_digits=10, decimal_places=2)
     quantity = models.PositiveIntegerField()
     is_active = models.BooleanField(default=True)
+
+    user = models.ForeignKey(
+        "users.User", on_delete=models.CASCADE, related_name="products"
+    )
