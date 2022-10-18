b3998aaf7dbba2d347a9c04cc6b26da5174b0f58
ErickMarchetti <erickgmarchetti@gmail.com>
2022-10-18 12:00:52 -0300

    feat: serializer detalhado e geral de products

---
+++ products/serializers.py
@@ -0,0 +1,33 @@
+from rest_framework import serializers
+
+from users.serializers import AccountSerializer
+
+from .models import Product
+
+
+class ProductDetailsSerializer(serializers.ModelSerializer):
+    seller = AccountSerializer(read_only=True)
+
+    class Meta:
+        model = Product
+        fields = "__all__"
+        read_only_fields = ["id"]
+
+
+class ProductGeneralSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = [
+            "description",
+            "price",
+            "quantity",
+            "is_active",
+            "seller_id",
+        ]
+        read_only_fields = [
+            "description",
+            "price",
+            "quantity",
+            "is_active",
+            "seller_id",
+        ]
