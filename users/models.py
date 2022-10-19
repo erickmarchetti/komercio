03c87ee1182b63b3c154abd256c440b79a4dc23a
ErickMarchetti <erickgmarchetti@gmail.com>
2022-10-19 13:58:05 -0300

    feat: adicionando id com uuid

--- users/models.py
+++ users/models.py
@@ -1,8 +1,10 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+import uuid
 
 
 class User(AbstractUser):
+    id = models.UUIDField(default=uuid.uuid4, primary_key=True, editable=False)
     username = models.TextField(unique=True)
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
